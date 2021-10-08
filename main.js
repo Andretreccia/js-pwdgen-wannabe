e08806f96ead915a107d4722de7c668e0024bdb1
Andretreccia <andreabtreccia@gmail.com>
2021-10-08 14:17:48 +0200

    Aggiunto elemento con ID nel file "index.html" Aggiunti script per la raccolta dati nel file "main.js"

---
+++ main.js
@@ -0,0 +1,18 @@
+/* Numero finale della password genarata */
+let last_number = "21"
+
+//Chiedi all'utente il suo nome
+
+const user_name = prompt("Ho bisogno di sapere il tuo nome")
+console.log("Nome: " + user_name)
+
+//Chiedi all'utente il suo cognome
+
+const user_surname = prompt("Ho bisogno di sapere il tuo Cognome")
+console.log("Cognome: " + user_surname)
+
+//Chiedi all'utente il suo colore preferito
+
+const user_color = prompt("Ho bisogno di sapere il tuo colore preferito")
+console.log("Colore preferito: " + user_color)
+
